b03b6ba94ef107927dcda8c8d02072c960f8370c
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-06 20:20:49 +0530

    simplest php code

---
+++ basic.php
@@ -0,0 +1,11 @@
+<html>
+    <head>        
+    </head>
+    <body>
+        <h1>PHP</h1>  
+         <!--creating a php tag  -->
+        <?php
+         echo("Namastey World🙏");
+         ?>
+        </body>
+</html>
